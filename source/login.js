1302da3fcc744a8ef9f7b71f505cc4fc5ac2c7e9
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-08-10 13:07:11 -0400

    Login giver error message when user submits a blank form.

--- source/login.js
+++ source/login.js
@@ -13,14 +13,52 @@ class login {
             const username = document.getElementById("username").value;
             const password = document.getElementById ("pass").value;
             console.log ("Username: " + username + " Password: " + password);
+
+
             // self.fields.forEach((field) => {
             //     const input = document.querySelector("#${field}");
             //     console.log(fields);
 
             // })
-        })
+
+            var error = 0;
+
+            //error messages
+            if (username == "" && password == "") {
+                error ++;
+                this.setStatus(username, "Please enter username and password", "error");
+                console.log ("An error has been recorded: both username and password are empty");
+            }
+
+            else if (username == "") {
+                error++;
+                this.setStatus(username, "Username cannot be blank", "error");
+                console.log ("An error has been recorded on username");
+            }
+
+            else if (password == "") {
+                error++;
+                this.setStatus(password, "Password cannot be blank. Please try again.", "error");
+                console.log ("An error has been recorded on password");
+            }
+
+            if (error == 0) {
+                console.log ("The form would be submitted in this case");
+                this.form.submit();
+            }
+        }) 
+    }
+
+    setStatus (field, message,status) {
+        const errorMessage = document.querySelector(".loginErrorMessage");
+        if (status == "error") {
+            errorMessage.innerHTML = message;
+            
+        }
     }
 }
+
+
 
 const form = document.querySelector (".login");
 //if the form is set, we want to pass the fields.
