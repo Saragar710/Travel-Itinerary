197a7834552eb7b8a8730aadea9723dd2f47d914
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-08-10 00:34:12 -0400

    Added javascript to toggle between login and create account form.

---
+++ source/loginPage.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("hidden");
+        createAccountForm.classList.remove("hidden");
+    })
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("hidden");
+        createAccountForm.classList.add("hidden");
+    })
+})
