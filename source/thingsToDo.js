83689275a94616983d61167d300dc6bc83ec7dbc
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-08-14 18:35:09 -0400

    Finished autofill bar when user looks things up. Results now pop up according to category.

--- source/thingsToDo.js
+++ source/thingsToDo.js
@@ -1,35 +1,68 @@
-console.log("Entered init js");
-
-
-
 function initMap() {
-    autocomplete = new google.maps.places.Autocomplete((document.getElementById("autocomplete")),
+    console.log("InitMap was entered");
+    
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById("autocomplete"),
     {
         types:["geocode"]
     });
 
-    autocomplete.addListener("placeChanged", searchNearbyPlaces);
+    autocomplete.addListener("place_changed", searchNearbyPlaces);
 
 }
 
 function searchNearbyPlaces() {
+
+    console.log("Entered SearchNearbyPlaces Function");
+
+    //resets table in case user enters a different location
     document.getElementById("places").innerHTML = "";
 
     var place = autocomplete.getPlace();
     console.log(place);
+
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: place.geometry.location,
+        zoom:15
+    });
+
+    console.log([document.getElementById("type").value]);
+
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: place.geometry.location,
+        radius: "5000",
+        type: [document.getElementById("type").value]
+        }, callback);
 }
 
-// // Construct the request URL
-// const apiUrl = https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=LATITUDE,LONGITUDE&radius=RADIUS&type=TYPE&key=YOUR_API_KEY;
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        console.log("There are this many results: " + results.length);
+        for (var i = 0; i< results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
 
-// // Make the API request
-// fetch(apiUrl)
-//   .then(response => response.json())
-//   .then(data => {
-//     // Handle the response data
-//     console.log(data);
-//   })
-//   .catch(error => {
-//     // Handle any errors
-//     console.error('Error:', error);
-//   });
+function createMarker(place) {
+    console.log(place);
+    var table = document.getElementById("places");
+    var row = table.insertRow();
+    var cell1 = row.insertCell(0);
+    cell1.innerHTML = (place.name + "<br><br>" + place.vicinity);
+
+    if(place.photos) {
+        const image = document.createElement("img");
+        image.src = place.photos[0].getUrl();
+        let cell2 = row.insertCell(1);
+        image.width = 300;
+        image.height = 200;
+        image.style.borderRadius = 50; 
+        cell2.innerHTML = image.outerHTML;
+        cell2.innerHTML.style.borderRadius = 50;
+    }
+        
+}
+
+document.getElementById("type").onchange = searchNearbyPlaces;
