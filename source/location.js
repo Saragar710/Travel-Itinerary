a6140453bafda0add46da08f433ee0b06f5fda06
Nicole <nicolejoan9@yahoo.com>
2023-08-15 18:04:29 -0400

    cleaned up code

--- source/location.js
+++ source/location.js
@@ -2,10 +2,6 @@ var startTripBtn = document.getElementById("startTripBtn");
 var whereToBox = document.getElementById("floatingInput");
 var startDate = document.getElementById("floatingInputGrid");
 var endDate = document.getElementById("floatingInputGrid2");
-<<<<<<< HEAD
-
-=======
->>>>>>> 416ae13f6875ff67d77ec0d324a0112e9784dfe2
 
 var hasWarning = false;
 var autocomplete;
@@ -19,16 +15,7 @@ function initAutocomplete() {
   );
 }
 
-var preferredDestination= {
-  location: whereToBox.value,
-  startDate:startDate.value,
-  endDate:endDate.value
-}
-console.log(preferredDestination);
-// var userData = JSON.stringify(preferredDestination);
 var searchCity = JSON.parse(window.localStorage.getItem("searchCity")) || [];
-
-var hasWarning = false;
 
 startTripBtn.addEventListener("click", function () {
   if (whereToBox.value === "") {
@@ -52,7 +39,5 @@ startTripBtn.addEventListener("click", function () {
  
 });
 var storedData = JSON.parse(localStorage.getItem("Chosen Location")) || [];
-  searchCity.push(preferredDestination);
-  localStorage.setItem("searchCity", JSON.stringify(preferredDestination));
-});
+
 
