54c6b1b02fcc24a5e3289cc519189bee3d2eecc0
Sara Garcia <saragca81@gmail.com>
2023-08-15 14:33:24 -0700

    added api

--- source/restaurant.js
+++ source/restaurant.js
@@ -21,26 +21,34 @@
 // response.addEventListener(response.data,'click', function(event){
  
 // })
-const url = 'https://worldwide-restaurants.p.rapidapi.com/search';
-const options = {
-	method: 'POST',
-	headers: {
-		'content-type': 'application/x-www-form-urlencoded',
-		'X-RapidAPI-Key': 'b6a9e3060amsha6d19ed1a224e1ep14dbb8jsn15df1069f330',
-		'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
-	},
-	body: new URLSearchParams({
-		language: 'en_US',
-		limit: '30',
-		location_id: '297704',
-		currency: 'USD'
-	})
-};
 
-try {
-	const response = await fetch(url, options);
-	const result = await response.text();
-	console.log(result);
-} catch (error) {
-	console.error(error);
-}
+// AIzaSyDa-NcckTIrTU8Qhz437JcSJoK30OswcGg //google api key
+
+
+
+
+
+
+// const url = 'https://worldwide-restaurants.p.rapidapi.com/search';
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'content-type': 'application/x-www-form-urlencoded',
+// 		'X-RapidAPI-Key': 'b6a9e3060amsha6d19ed1a224e1ep14dbb8jsn15df1069f330',
+// 		'X-RapidAPI-Host': 'worldwide-restaurants.p.rapidapi.com'
+// 	},
+// 	body: new URLSearchParams({
+// 		language: 'en_US',
+// 		limit: '30',
+// 		location_id: '297704',
+// 		currency: 'USD'
+// 	})
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
