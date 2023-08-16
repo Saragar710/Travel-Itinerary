85c1cf1491ed9e45a22503ecbf527e08f7accca8
Alejandra Camacho <alejandra.e.bernabel@gmail.com>
2023-08-16 12:22:16 -0400

    Login now takes you to location.html if there is no set destination. Takes you to navbar.html if there is a set location.

--- source/login.js
+++ source/login.js
@@ -41,7 +41,19 @@ class login {
             if (error == 0) {
                 console.log ("The form would be submitted in this case");
                 localStorage.setItem("auth",1);
-                this.form.submit();
+                //this.form.submit();
+
+                //if the user has not chosen a destination, they are taken to location page
+                //if user has chosen a distination, they are taken to navbar.html
+                if (localStorage.getItem("chosen Location")==null){
+                    // console.log(localStorage.getItem("chosen Location"));
+                    window.location.href= "location.html";
+                }
+                
+                else {
+                    // console.log(localStorage.getItem("chosen Location"));
+                    window.location.href= "navbar.html";
+                }
                 // window.location.href= "location.html";
             }
         }) 
