0859d5df4ac576d449c65ef5b08dfd96d6a4b8d3
Sara Garcia <saragca81@gmail.com>
2023-08-16 18:34:37 -0700

    added code

--- source/restaurant.js
+++ source/restaurant.js
@@ -34,7 +34,7 @@
 // 	console.error(error);
 // 
   
-
+debugger;
     function initMap() {
         autocomplete = new google.maps.places.Autocomplete((document.getElementById('autocomplete')),
         {
