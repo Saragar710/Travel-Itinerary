e69b112f60557824446add9b71581aeb9f5499dc
Abigail Burtley <abigailtop95@yahoo.com>
2023-08-17 13:47:38 -0400

    add jquery functions to be compatible with bootstrap modals pop ups. Function set to save budget which user will enter, when user refresh the page, the budget or changes will stay displayed unless updated by user. Function set to display expense details , user must fill out the select and amount expense inputs to submit, form, budget notes are an option to add.

--- itinerary.js
+++ itinerary.js
@@ -1,6 +1,53 @@
+$(document).ready(function() {
+  const storedInputValue = localStorage.getItem('budgetAmount'); //stored budget will be shown and displayed when user refresh page
+  if(storedInputValue) {
+    $('#budget-track').text('Budget:' + '$' + storedInputValue);
+  }
+  $('#save-budget-amount').on('click', function(event){ //user input budget amount and saved in local storage
+   event.preventDefault();
+   const newBudgetAmount = $('#budgetdollar-amount').val();
+   $('#budget-track').text('Budget:' + '$' + storedInputValue) //the area or element to display the budget input
+ 
+  localStorage.setItem('budgetAmount', newBudgetAmount);
+  //close modal
+  $('#edit-budget-modal').modal('hide');
+  console.log(newBudgetAmount);
+  
+});
+   $('#save-budget-amount').on('click', function(event){
+   event.preventDefault();
+  var inputBudgetEl = $('#budgetdollar-amount').val();
+  localStorage.setItem('budgetAmount', inputBudgetEl);
+  //close modal
+  $('#edit-budget-modal').modal('hide');
+  console.log(inputBudgetEl);
+ 
+    const storedInputValue = localStorage.getItem('budgetAmount');
+    if (storedInputValue) {
+        // Do something with the stored input value
+        $('#budget-track').text('Budget: ' + '$' + storedInputValue);
+        let budgetDisplay = $('#budget-track').val();
+        localStorage.setItem('budgetShown', budgetDisplay);
+        
+    };
+   
+
+   });
+    $('#save-expense-details').on('click', function(event){
+    event.preventDefault();
+    let selectedExpense = $('#expense-list-name').val();
+    $('#add-expense-details').modal('hide');
+    console.log(selectedExpense);
+  });
+ 
+});
+
+
+
+
+
 var acc = document.getElementsByClassName("accordion");
 var i;
-
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     /* Toggle between adding and removing the "active" class,
