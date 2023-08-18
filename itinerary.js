73b8fb16bc4437cf2df1f6f9367fd1d560f3367d
Abigail Burtley <abigailtop95@yahoo.com>
2023-08-17 23:19:37 -0400

    add fix to add budget button

--- itinerary.js
+++ itinerary.js
@@ -2,38 +2,35 @@ $(document).ready(function() {
   const storedInputValue = localStorage.getItem('budgetAmount'); //stored budget will be shown and displayed when user refresh page
   if(storedInputValue) {
     $('#budget-track').text('Budget:' + '$' + storedInputValue);
+    
   }
-  $('#save-budget-amount').on('click', function(event){ //user input budget amount and saved in local storage
+
+  $('#save-budget-amount').on('click', function(event) { //user input budget amount and saved in local storage
    event.preventDefault();
-   const newBudgetAmount = $('#budgetdollar-amount').val();
+   var newBudgetAmount = $('#budgetdollar-amount').val();
    $('#budget-track').text('Budget:' + '$' + storedInputValue) //the area or element to display the budget input
  
   localStorage.setItem('budgetAmount', newBudgetAmount);
-  //close modal
-  $('#edit-budget-modal').modal('hide');
-  console.log(newBudgetAmount);
   
-});
-   $('#save-budget-amount').on('click', function(event){
-   event.preventDefault();
-  var inputBudgetEl = $('#budgetdollar-amount').val();
-  localStorage.setItem('budgetAmount', inputBudgetEl);
-  //close modal
   $('#edit-budget-modal').modal('hide');
-  console.log(inputBudgetEl);
- 
-    const storedInputValue = localStorage.getItem('budgetAmount');
+  console.log(newBudgetAmount)
+  });
+});
+/*
+  const storedInputValue = localStorage.getItem('budgetAmount');
     if (storedInputValue) {
         // Do something with the stored input value
         $('#budget-track').text('Budget: ' + '$' + storedInputValue);
+    }
+  });
+});
         let budgetDisplay = $('#budget-track').val();
         localStorage.setItem('budgetShown', budgetDisplay);
         
-    };
-   
+    
 
-   });
-    $('#save-expense-details').on('click', function(event){
+
+   $('#save-expense-details').on('click', function(event){
     event.preventDefault();
     let selectedExpense = $('#expense-list-name').val();
     let noSelection = $('#select-result');
@@ -60,6 +57,7 @@ $(document).ready(function() {
     if (selectedExpense !== "" && userInputAmount !== "") {
       localStorage.setItem('selectedExpense', selectedExpense);
       localStorage.setItem('userInputAmount', userInputAmount);
+      $('#save-expense-details').modal('hide');
 
       const expenseContainer = $('#add-expense-js');
       for (let i = 0; i < 5; i++) {
@@ -69,24 +67,22 @@ $(document).ready(function() {
         });
         
         expenseContainer.append(newExpenseDiv);
-        
-      }
-     let totalExpenses = 0;
+      
+    
+      let totalExpenses = 0;
     for(let i =0; i < expenses.length; i++) {
+      
+  
       totalExpenses += parseFloat(expenses[i].amount);
-    }
+    
     
     const progressBarWidth = (totalExpenses / totalBudget) * 100;
     $('#progress-bar').css('width', progressBarWidth + '%');
     $('#total-expenses').text('$' + totalExpenses.toFixed(2));
       
-      $('#add-expense-details').modal('hide');
-    }
-   
-    });
-  
-      
-  });
+     
+    
+    
   
 
 
@@ -94,19 +90,18 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
+     Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel 
     this.classList.toggle("active");
 
-    /* Toggle between hiding and showing the active panel */
+    Toggle between hiding and showing the active panel 
     var panel = this.nextElementSibling; //next sibling is the p-tag, which will hold the expense list
     if (panel.style.display === "block") {
       panel.style.display = "none";
     } else {
       panel.style.display = "block";
     }
-  });
-}
+  
 
 function copyText() {
       
@@ -114,4 +109,4 @@ function copyText() {
   //copy invite link 
   navigator.clipboard.writeText
       ("https://github.com/Saragar710/Travel-Itinerary");
-}
+*/
